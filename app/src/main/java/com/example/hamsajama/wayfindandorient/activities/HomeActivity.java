f40c068c189c84b6d1b7c747b43d1baf39a69f59
Ahassan204 <ahassan204@gmail.com>
2016-08-14 15:17:49 +0100

    Added Motion Detection
    Added Motion Detection class

--- app/src/main/java/com/example/hamsajama/wayfindandorient/activities/HomeActivity.java
+++ app/src/main/java/com/example/hamsajama/wayfindandorient/activities/HomeActivity.java
@@ -1,20 +1,37 @@
 package com.example.hamsajama.wayfindandorient.activities;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.hamsajama.wayfindandorient.R;
+import com.example.hamsajama.wayfindandorient.services.ActivityRecognizedService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
 
 /**
  * Created by hamsajama on 12/08/2016.
  */
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public GoogleApiClient mApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(ActivityRecognition.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mApiClient.connect();
     }
     public void startUserActivity(View view) {
         Intent userActivity = new Intent(HomeActivity.this, UserActivity.class);
@@ -25,5 +42,23 @@ public class HomeActivity extends AppCompatActivity {
         Intent carerActivity = new Intent(HomeActivity.this, CarerActivity.class);
         startActivity(carerActivity);
         overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+    }
+
+    //--Added code to make
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Intent intent = new Intent( this, ActivityRecognizedService.class );
+        PendingIntent pendingIntent = PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( mApiClient, 3000, pendingIntent );
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
